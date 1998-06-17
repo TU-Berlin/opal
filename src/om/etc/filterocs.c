c4fb7fdf5a02e325bb8ce957098d0dfe6a2e9284
Klaus Didrich <kd@cs.tu-berlin.de>
1998-06-17 11:39:12 +0000

    standard header und noting -> not*h*ing

--- src/om/etc/filterocs.c
+++ src/om/etc/filterocs.c
@@ -3,8 +3,9 @@
 /* Copyright 1989 - 1998 by the Opal Group, TU Berlin. All rights reserved 
    See OCSHOME/etc/LICENSE or 
    http://uebb.cs.tu-berlin.de/~opal/LICENSE.html for details
-   $Date: 1998-06-17 11:01:14 $ ($Revision: 1.2 $)
+   $Date: 1998-06-17 11:39:12 $ ($Revision: 1.3 $)
 */
+
 #include <string.h>
 #include <stdio.h>
 
