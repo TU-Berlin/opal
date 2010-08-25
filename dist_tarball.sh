4a15db13cdd5867abfae98db406073b73199266b
Florian Lorenzen <florian.lorenzen@tu-berlin.de>
2010-08-25 14:12:27 +0000

    Draft script to create distribution tarball.

---
+++ dist_tarball.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cd ${0%/*}
+
+
+. src/om/specs/ShSpecs.basic
+
+
+tmpdir=`mktemp -d`
+
+echo Using temporary directory $tmpdir
+
+distdir=$tmpdir/ocs-$VERSION
+tarball=ocs-$VERSION.tar.gz
+
+mkdir $distdir
+
+# Create C and intermediate files.
+ocs
+
+echo "Copying files to temporary directory..."
+tar c --exclude=$tarball --exclude=.svn -f - . | tar x -C $distdir -f -
+
+echo "Removing unnecessary files..."
+find $distdir \( -name \*.o -o -name \*.a \) -exec rm {} \;
+
+for f in `cat DELETE.exe`; do
+    rm $distdir/$f
+done
+for f in `cat DELETE.ocsdefs`; do
+    rm $distdir/$f
+done
+for f in `cat DELETE.OCS`; do
+    rm -r $distdir/$f
+done
+
+echo "Writing dummy intermediate files..."
+for f in `find $distdir -name \*.inter -o -name \*.opt`; do
+    mtime="`stat -c \"%y\" $f`"
+    echo "Dummy intermediate file to enable bootstrapping. Do not change its mtime!" > $f
+    touch -d "$mtime" $f
+done
+
+echo "Creating tarball $tarball..."
+tar vcz $tarball -C $tmpdir ocs-$VERSION
+
+echo "Deleting temporary firectory..."
+rm -rf $TMP
