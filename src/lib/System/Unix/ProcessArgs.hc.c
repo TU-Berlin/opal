466bd6c3cb4c6e88e6793ec4b15d72a7ce671690
Christoph Hoeger <christoph.hoeger@tu-berlin.de>
2013-10-09 09:48:25 +0000

    Add explicit casts to char*

--- src/lib/System/Unix/ProcessArgs.hc.c
+++ src/lib/System/Unix/ProcessArgs.hc.c
@@ -37,7 +37,7 @@ extern OBJ _AProcessArgs_AcSetArgs(OBJ NewArgs,OBJ Unit){
 
 extern OBJ _AProcessArgs_AcEnv(OBJ Var,OBJ Unit) {
     char *val;
-    val = getenv(data_denotation(Var));
+    val = getenv((char*)data_denotation(Var));
     free_denotation(Var, 1);
     if (val != NULL){
 	return_okay(make_denotation(val));
@@ -55,7 +55,7 @@ extern OBJ _AProcessArgs_AcPutEnv(OBJ Def,OBJ Unit){
     if (buf == NULL) {
       HLT("cPutEnv'ProcessArgs.hc.c: out of memory");
     };
-    strncpy(buf, data_denotation(Def), leng_denotation(Def) + 1);
+    strncpy(buf, (char*)data_denotation(Def), leng_denotation(Def) + 1);
     res = putenv(buf);
     free_denotation(Def, 1);
     if (res == 0){
